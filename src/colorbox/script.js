a754eea48daf124d9527532fbc5fa87b2ad0b0f9
kaiclastic <halajash567@outlook.com>
2024-10-24 15:22:10 +0000

    finished dom workshop

--- src/colorbox/script.js
+++ src/colorbox/script.js
@@ -1,1 +1,5 @@
 // TODO: add an event listener to the .box element that toggles the .red class
+const box = document.querySelector(".box")
+console.dir(box)
+
+box.classList.toggle(".red")
--- src/counter/script.js
+++ src/counter/script.js
@@ -1,1 +1,16 @@
 // TODO: increment and decrement the count when the buttons are clicked
+const counter = document.querySelector(".counter")
+console.dir(counter)
+const count = document.querySelector("#count")
+
+const increment = document.querySelector("#increment")
+const decrement = document.querySelector("#decrement")
+
+increment.addEventListener("click",()=>{
+    count.innerText ++;
+})
+
+decrement.addEventListener("click",()=>{
+    count.innerText --
+})
+
